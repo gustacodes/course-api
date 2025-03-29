2cd2273fcdd7e3187cd73ce5f8c748495339fefa
Gustavo Henrique <gustalencarvalho@gmail.com>
2025-03-29 00:04:42 -0300

    feat: implemented verification before delete course

--- src/main/java/com/ead/course/repositories/CourseUserRespository.java
+++ src/main/java/com/ead/course/repositories/CourseUserRespository.java
@@ -3,9 +3,14 @@ package com.ead.course.repositories;
 import com.ead.course.models.CourseModel;
 import com.ead.course.models.CourseUserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CourseUserRespository extends JpaRepository<CourseUserModel, UUID> {
     boolean existsByCourseAndUserId(CourseModel courseModel, UUID userId);
+    @Query(value = "SELECT * FROM TB_COURSES_USERS WHERE COURSE_COURSE_ID = :courseId", nativeQuery = true)
+    List<CourseUserModel> findAllCourseUserIntoCourse(@Param("courseId") UUID courseId);
 }
--- src/main/java/com/ead/course/services/Impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/Impl/CourseServiceImpl.java
@@ -1,9 +1,11 @@
 package com.ead.course.services.Impl;
 
 import com.ead.course.models.CourseModel;
+import com.ead.course.models.CourseUserModel;
 import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.CourseUserRespository;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
@@ -30,10 +32,14 @@ public class CourseServiceImpl implements CourseService {
     @Autowired
     private LessonRepository lessonRepository;
 
+    @Autowired
+    private CourseUserRespository courseUserRespository;
+
     @Transactional
     @Override
     public void delete(CourseModel courseModel) {
         List<ModuleModel> moduleModelList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+
         if (!moduleModelList.isEmpty()) {
             for (ModuleModel module : moduleModelList) {
                 List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(module.getModuleId());
@@ -43,6 +49,13 @@ public class CourseServiceImpl implements CourseService {
             }
             moduleRepository.deleteAll(moduleModelList);
         }
+
+        List<CourseUserModel> courseUserModelList = courseUserRespository.findAllCourseUserIntoCourse(courseModel.getCourseId());
+
+        if (!courseUserModelList.isEmpty()) {
+            courseUserRespository.deleteAll(courseUserModelList);
+        }
+
         courseRepository.delete(courseModel);
     }
 
